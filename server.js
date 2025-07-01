76a9b726c98f074cd9d6d732cbe8c46ca9bb454c
Gateau48 <miereprince48@gmail.com>
2025-07-01 23:08:13 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/generate', async (req, res) => {
+  const { prenom, message } = req.body;
+
+  // Charger le template HTML
+  let template = fs.readFileSync('template.html', 'utf8');
+  template = template
+    .replace('{{prenom}}', prenom)
+    .replace('{{message}}', message);
+
+  const browser = await puppeteer.launch({
+  args: chromium.args,
+  defaultViewport: chromium.defaultViewport,
+  executablePath: await chromium.executablePath,
+  headless: chromium.headless,
+});
+
+  const page = await browser.newPage();
+  await page.setContent(template, { waitUntil: 'networkidle0' });
+  const pdfBuffer = await page.pdf({ format: 'A4' });
+  await browser.close();
+
+  res.set({
+    'Content-Type': 'application/pdf',
+    'Content-Disposition': 'inline; filename="citation.pdf"',
+  });
+
+  res.send(pdfBuffer);
+});
+
+app.listen(3000, () => {
+  console.log('Serveur en ligne sur le port 3000');
+});
